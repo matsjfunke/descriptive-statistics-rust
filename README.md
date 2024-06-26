c25d037229a1c0be83374e8ab518482cae729243
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-06-26 02:42:07 +0200

    improved plot and README

--- README.md
+++ README.md
@@ -2,4 +2,31 @@
 - on the [Kaggle dataset: UEFA EURO 2024 - Players](https://www.kaggle.com/datasets/damirdizdarevic/uefa-euro-2024-players)
   
 ## plotting market-value agaist scored national team goals
-![plot](scatter_plot.png)
+![plot](images/labeled_scatter.png)
+
+## Usage
+```bash
+# clone repo
+git clone https://github.com/matsjfunke/descriptive-statistics-rust.git
+# run main.rs
+cargo run
+# view top 10 most valueable players in the euro 2024
+# view top 10 best national goal scorers of prior to euro 2024
+# view plot
+open images/scatter_plot.png
+```
+
+## Project Structure
+```
+├── Cargo.lock
+├── Cargo.toml -> dependencies
+├── README.md
+├── euro2024_players.csv -> dataset
+├── images
+│   ├── labeled_scatter.png
+│   └── scatter_plot.png
+└── src
+    ├── main.rs -> entrypoint & dataset processing (csv to rust vector)
+    ├── models.rs -> defines data structures
+    └── top_ten.rs -> function for printing top 10 players
+```
--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn print_all_players(players: &[Player]) {
 
 // Function to plot scatter plot
 fn plot_scatter(players : &[Player]) -> Result<(), Box<dyn Error>> {
-    let root_area = BitMapBackend::new("scatter_plot.png", (1000, 800)).into_drawing_area();
+    let root_area = BitMapBackend::new("images/scatter_plot.png", (1000, 800)).into_drawing_area();
     root_area.fill(&WHITE)?;
 
     let mut chart = ChartBuilder::on(&root_area)
@@ -43,13 +43,13 @@ fn plot_scatter(players : &[Player]) -> Result<(), Box<dyn Error>> {
         .margin(20)
         .x_label_area_size(40)
         .y_label_area_size(40)
-        .build_cartesian_2d(0..50, 0..100_000_000)?;
+        .build_cartesian_2d(0..100,1_000_000..180_000_000)?;
 
     chart.configure_mesh().draw()?;
 
     // Draw the scatter plot
     chart.draw_series(
-        players.iter().map(|player| Circle::new((player.goals, player.market_value), 3, BLUE.filled())),
+        players.iter().map(|player| Circle::new((player.goals, player.market_value), 3, RED.filled())),
     )?;
 
     Ok(())
